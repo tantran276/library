0968c196661d15eafa7baab49e5ef4b56d0925d7
Tran Minh Tan <tan.tran27620@gmail.com>
2022-05-05 11:31:17 +0700

    First Commit

---
+++ TTTN-library-managerment-3/src/main/java/com/example/tttn/TttnLibraryManagerment3Application.java
@@ -0,0 +1,13 @@
+package com.example.tttn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttnLibraryManagerment3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TttnLibraryManagerment3Application.class, args);
+	}
+
+}
