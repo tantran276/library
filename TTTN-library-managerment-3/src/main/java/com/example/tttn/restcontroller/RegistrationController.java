9b86dd50a749f95d558c33bedbfd17d76439f555
Tran Minh Tan <tan.tran27620@gmail.com>
2022-05-07 08:05:44 +0700

    Bad request status in register api

--- TTTN-library-managerment-3/src/main/java/com/example/tttn/restcontroller/RegistrationController.java
+++ TTTN-library-managerment-3/src/main/java/com/example/tttn/restcontroller/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.example.tttn.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.tttn.payload.request.RegistrationRequest;
-import com.example.tttn.service.RegistrationService;
 import com.example.tttn.service.UserService;
 
 @RestController
@@ -18,11 +18,19 @@ public class RegistrationController {
 	@Autowired
 	UserService userService;
 	
-	@Autowired
-	RegistrationService registrationService;
 	
 	@PostMapping
-	public ResponseEntity<?> register(@RequestBody RegistrationRequest request) {
-		return ResponseEntity.ok().body(registrationService.register(request));
+	public ResponseEntity<?> register(@RequestBody RegistrationRequest request) {		
+		if (!userService.validateEmail(request.getEmail())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email không hợp lệ");
+		}
+		if (userService.existsByEmail(request.getEmail())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email đã tồn tại");
+		}
+		if (userService.existsByUsername(request.getUsername())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tên tài khoản đã tồn tại!");
+		}
+		userService.createUser(request);
+		return ResponseEntity.ok().body("Đăng ký thành công");
 	}
 }
--- TTTN-library-managerment-3/src/main/java/com/example/tttn/service/UserService.java
+++ TTTN-library-managerment-3/src/main/java/com/example/tttn/service/UserService.java
@@ -1,17 +1,15 @@
  package com.example.tttn.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.tttn.entity.User;
+import com.example.tttn.mapper.RegistrationRequestToUser;
+import com.example.tttn.payload.request.RegistrationRequest;
 import com.example.tttn.repository.UserRepository;
 import com.example.tttn.security.PasswordEncoder;
 
@@ -24,6 +22,9 @@ public class UserService implements UserDetailsService{
 
 	@Autowired
 	PasswordEncoder passwordEncoder;
+	
+	private EmailValidator emailValidator = EmailValidator.getInstance();
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
@@ -31,21 +32,27 @@ public class UserService implements UserDetailsService{
 			new UsernameNotFoundException(String.format(USER_NOT_FOUND, username)));
 	}
 	
-	public String signUpUser(User user) {
-		if(userRepository.existsByEmail(user.getEmail())) {
-			return "Email đã tồn tại!";
-		}
-		if (userRepository.existsByUsername(user.getUsername())) {
-			return "Tên đăng nhập đã tồn tại!";
-		}
-		String encodedPassword = passwordEncoder.bCryptPasswordEncoder().encode(user.getPassword());
-		user.setPassword(encodedPassword);
-		userRepository.save(user);
-		return "Đăng ký thành công";
-	}
-	
 	public User findByUsername (String username) {
 		return userRepository.findByUsername(username).orElseThrow(() ->
 			new UsernameNotFoundException(String.format(USER_NOT_FOUND, username)));
+	}
+	
+	public boolean existsByEmail(String email) {
+		return userRepository.existsByEmail(email);
+	}
+	
+	public boolean existsByUsername(String username) {
+		return userRepository.existsByUsername(username);
+	}
+	
+	public User createUser(RegistrationRequest request) {
+		User user = RegistrationRequestToUser.toUser(request);
+		String encodedPassword = passwordEncoder.bCryptPasswordEncoder().encode(user.getPassword());
+		user.setPassword(encodedPassword);
+		return userRepository.save(user);
+	}
+	
+	public boolean validateEmail(String email) {
+		return emailValidator.isValid(email);
 	}
 }
